fcce0e944353ae9ac29fb5eef90eb8ce6b2b2782
Brandon Garcia <19005926@galileo.edu>
2021-10-21 00:47:13 -0600

    Configurando hostname y port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const hostname = process.env.HOST;
 const app = require('src/config/server');
 require('src/app/routes/users') (app);
 
 app.listen(app.get('port'), 
-    () => console.log(`server corriendo en puerto ${app.get('port')}.`));
+    () => console.log(`server corriendo en ${hostname}, en el puerto ${app.get('port')}.`));
